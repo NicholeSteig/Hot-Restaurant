d8eb70e3d87114f082a90726108901f313f1a324
Nyx <nichole.steig@gmail.com>
2019-11-23 12:34:05 -0800

    working on server.js

--- server.js
+++ server.js
@@ -10,23 +10,39 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// //Tables data
-// const data = [
-//     {
-//         "name" : "John",
-//         "email": "john@gmail.com"
-//     },
-// ]
+//Tables data
+const data = [
+    {
+        "name": "John",
+        "phone": "123-456-7890",
+        "email": "john@gmail.com",
+        "id": "hellooo"
+    },
+]
 
 
 
  // Basic route that sends the user first to the AJAX Page
  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "home.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
-app.get("/table", function(req, res) {
-    res.sendFile(path.join(__dirname, "table.html"));
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
 });
 app.get("/reserve", function(req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
+
+app.post("/tables", function(req, res) {
+var newReserve = req.body;
+
+console.log(newReserve);
+
+table.push(newReserve);
+
+res.json(newReserve);
+});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
